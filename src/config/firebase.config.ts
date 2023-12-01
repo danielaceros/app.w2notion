1c9e01b18c6d03338ac335c03f051485587910e6
Daniel Acero Sagredo <danielacerosagredos@gmail.com>
2023-12-01 19:08:48 +0100

    prueba2

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyB5Xk-Acp9QPzuh8nbQ2b01VFGQMRYlP9w",
-    authDomain: "emailx-ai.firebaseapp.com",
-    projectId: "emailx-ai",
-    storageBucket: "emailx-ai.appspot.com",
-    messagingSenderId: "602228732982",
-    appId: "1:602228732982:web:52009ad54470bd559b1497",
-    measurementId: "G-LMPJWK5XLQ"
-  };
+  apiKey: "AIzaSyB6hjscAYL7gwgUsGXdNz-astZW6cRIors",
+  authDomain: "wh2notion.firebaseapp.com",
+  projectId: "wh2notion",
+  storageBucket: "wh2notion.appspot.com",
+  messagingSenderId: "99330018432",
+  appId: "1:99330018432:web:5b18076c8ce5c803dd6a65",
+  measurementId: "G-FLKKTW3RF1"
+};
